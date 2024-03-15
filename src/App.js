409d2484bb6a590b5f80bcc0fcb0fb665bf0cec8
rajeshkosta <155640944+rajeshkosta@users.noreply.github.com>
2024-03-15 14:03:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO THE AIEZE.
+          HII HOW ARE YOU.
         </p>
         <a
           className="App-link"
